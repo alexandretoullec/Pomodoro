de864cce28b5ea3299b6cfecf49b8bde5ae61e45
alexandretoullec <69418272+alexandretoullec@users.noreply.github.com>
2022-01-03 13:39:04 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const btnReset = document.querySelector('.b3')
 
 const cycles = document.querySelector('h2');
 
+let checkInterval = false;
+
 let tempsInitial = 1800;
 let tempsDeRepos = 300;
 let pause = false;
@@ -16,3 +18,67 @@ cycles.innerText = `nombre de cycles ${nbDeCycles}`
 affichageTravail.innerText = `${Math.trunc(tempsInitial / 60)} : ${(tempsInitial % 60 < 10) ? `0${tempsInitial % 60} ` : tempsInitial % 60}`; 
 
 affichagePause.innerText = `${Math.trunc(tempsDeRepos / 60)} : ${(tempsDeRepos % 60 < 10) ? `0${tempsDeRepos % 60} ` : tempsDeRepos % 60}`; 
+
+btnGo.addEventListener('click', ()=>{
+
+
+    if(checkInterval === false){
+
+    checkInterval = true
+    
+    tempsInitial--;
+    affichageTravail.innerText = `${Math.trunc(tempsInitial / 60)} : ${(tempsInitial % 60 < 10) ? `0${tempsInitial % 60} ` : tempsInitial % 60}`; 
+
+    let timer = setInterval(()=> {
+
+        if(pause === false  && tempsInitial > 0){
+            tempsInitial--;
+            affichageTravail.innerText = `${Math.trunc(tempsInitial / 60)} : ${(tempsInitial % 60 < 10) ? `0${tempsInitial % 60} ` : tempsInitial % 60}`; 
+        }
+        else if(pause === false && tempsInitial === 0 && tempsDeRepos === 0){
+            tempsInitial=1800;
+            tempsDeRepos=300;
+            nbDeCycles++;
+            affichageTravail.innerText = `${Math.trunc(tempsInitial / 60)} : ${(tempsInitial % 60 < 10) ? `0${tempsInitial % 60} ` : tempsInitial % 60}`; 
+
+            affichagePause.innerText = `${Math.trunc(tempsDeRepos / 60)} : ${(tempsDeRepos % 60 < 10) ? `0${tempsDeRepos % 60} ` : tempsDeRepos % 60}`; 
+            
+            cycles.innerText = `nombre de cycles ${nbDeCycles}`
+        }
+        else if (pause === false && tempsInitial === 0){
+            tempsDeRepos--;
+            affichagePause.innerText = `${Math.trunc(tempsDeRepos / 60)} : ${(tempsDeRepos % 60 < 10) ? `0${tempsDeRepos % 60} ` : tempsDeRepos % 60}`; 
+        }
+
+    },1000)
+
+
+    //reset
+    btnReset.addEventListener('click', ()=> {
+        clearInterval(timer);
+        checkInterval = false;
+        tempsInitial = 1800;
+        tempsDeRepos = 300;
+        affichageTravail.innerText = `${Math.trunc(tempsInitial / 60)} : ${(tempsInitial % 60 < 10) ? `0${tempsInitial % 60} ` : tempsInitial % 60}`; 
+
+            affichagePause.innerText = `${Math.trunc(tempsDeRepos / 60)} : ${(tempsDeRepos % 60 < 10) ? `0${tempsDeRepos % 60} ` : tempsDeRepos % 60}`; 
+    })
+
+}else{
+    return;}
+
+
+    
+    
+})
+
+btnPause.addEventListener('click',() => {
+
+    if(pause === false){
+        btnPause.innerText = 'Play';
+    }else if (pause === true){
+        btnPause.innerText = 'Pause'
+    }
+
+    pause = !pause; 
+})
